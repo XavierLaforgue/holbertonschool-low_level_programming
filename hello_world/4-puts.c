c34ccf494d605e5b8d62fde7d71d98bb4997a93e
XavierLaforgue <xavierjimmys@gmail.com>
2025-02-18 12:35:02 +0100

    C program that prints a string exactly followed by a new line

---
+++ hello_world/4-puts.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/** 
+ * main prints the string "req_string" exactly
+ * 
+ * req_string: a string with a special character (")
+ *  
+ * return: 0
+ */
+
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+
+return (0);
+}
