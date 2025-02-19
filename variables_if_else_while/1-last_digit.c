98c4e4dffaf6c4bbde5789d9c44e2b4897283c4f
XavierLaforgue <xavierjimmys@gmail.com>
2025-02-19 09:45:25 +0100

    Assign a random number to variable n each time it is executed and print the last number stored in n. Print the last digit and if it is 0, less than 6 or larger than 5

---
+++ variables_if_else_while/1-last_digit.c
@@ -0,0 +1,32 @@
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+#include <stdio.h>
+
+/* betty style doc for function main goes there */
+/**
+ * main - assign a random number to the variable n each time it is executed and
+ * print the last digit stored in n
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/* your code goes there */
+	int nmod10 = n % 10;
+	printf("n mod 10 is %d\n", nmod10);
+	printf("Last digit of %d is %d and is ", n, nmod10);
+	if (n == 0)
+		printf("0\n");
+	else if (n < 6)
+		printf("less than 6 and not 0\n");
+	else if (n > 5)
+	printf("greater than 5\n");
+
+ 	return (0);
+} 
