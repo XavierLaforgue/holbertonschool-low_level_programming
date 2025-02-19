52e46d0ff7f3eef3ceb90f2b5c646615792ce555
XavierLaforgue <xavierjimmys@gmail.com>
2025-02-19 16:32:21 +0100

    Print "_putchar" followed by a new line

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - prints "_putchar" followed by a new line
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+
+	return (0);
+}
---
+++ functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+Week 3, project 3: functions and nested loops
