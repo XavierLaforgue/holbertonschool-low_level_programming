ea876f90fdb550a0197f739ad5378f92569dcff6
XavierLaforgue <xavierjimmys@gmail.com>
2025-02-21 16:38:52 +0100

    Fix input for _putchar in print_last_digit

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -7,13 +7,21 @@
  * Return: the value of the last digit
  */
 
+int _abs(int s)
+{
+	if (s >= 0)
+		return (s);
+	else
+		return (-s);
+}
+
 int print_last_digit(int n)
 {
 	unsigned int r;
 
 	r = _abs(n);
 	r = r%10;
-	_putchar((char)r);
+	_putchar('0'+r);
 
 	return (r);
 }
