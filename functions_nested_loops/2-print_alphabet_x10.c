ed6c6600bb1e2fda081f7a4779a09af8ece08621
XavierLaforgue <xavierjimmys@gmail.com>
2025-02-21 14:14:58 +0100

    Betty compliance

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -1,7 +1,8 @@
 #include "main.h"
 
 /**
- * print_alphabet - prints the alphabet, in lowercase, followed by a new line
+ * print_alphabet_x10 - prints 10 times the alphabet,
+ * in lowercase, followed by a new line
  *
  * Return: Always 0
  */
@@ -9,9 +10,11 @@
 void print_alphabet_x10(void)
 {
 	int i;
-	for (i = 1; i<=10; ++i)
+
+	for (i = 1; i <= 10; ++i)
 	{
 		char i = 'a';
+
 		while (i <= 'z')
 		{
 			_putchar(i);
