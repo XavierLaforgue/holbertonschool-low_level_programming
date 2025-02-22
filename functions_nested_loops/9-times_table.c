e9c2694f34f59856f6b9a35133cc80dc063aeaf8
XavierLaforgue <xavierjimmys@gmail.com>
2025-02-22 15:01:09 +0100

    Prints the 9 times table, starting with 0

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -12,27 +12,21 @@ void times_table(void)
 
 	for (i = 0; i <= 9; ++i)
 	{
-		for (j = 0; j <= 9; ++j)
+		_putchar('0');
+		_putchar(',');
+		_putchar(' ');
+		for (j = 1; j <= 9; ++j)
 		{
 			p = i * j;
 			first_dig = p / 10;
 			last_dig = p % 10;
-			if (j == 0)
+			(first_dig == 0) ? _putchar(' ') :	_putchar('0' + first_dig);
+			_putchar('0' + last_dig);
+			if (j != 9)
 			{
-				_putchar('0');
-			}
-			else if (first_dig != 0)
-			{
-				_putchar('0' + first_dig);
-				_putchar('0' + last_dig);
-			}
-			else
-			{
+				_putchar(',');
 				_putchar(' ');
-				_putchar('0' + last_dig);
 			}
-			_putchar(',');
-			_putchar(' ');
 		}
 		_putchar('\n');
 	}
