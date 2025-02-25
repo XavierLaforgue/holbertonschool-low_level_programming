882a4547efd72dd8ad362c0d68a689fba57693d6
XavierLaforgue <xavierjimmys@gmail.com>
2025-02-25 17:14:08 +0100

    Return the length of a string

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+/**
+ * _strlen - extracts the length of a string
+ * @s: character string
+ *
+ * Return: the length of the string
+ */
+
+int _strlen(char *s)
+{
+	int size;
+
+	for (size = 0; *(s + size) != '\0'; size++)
+	{
+	}
+	return (size);
+}
