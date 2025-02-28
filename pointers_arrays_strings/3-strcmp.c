b33d93e23005744918873fd10f45b0be6d6ea94b
XavierLaforgue <xavierjimmys@gmail.com>
2025-02-28 16:28:56 +0100

    Compare two strings like strcmp

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -1,12 +1,14 @@
 #include "main.h"
 
 /**
- * _strcmp - compares two strings excalty like strcmp
+ * _strcmp - compares two strings exactly like strcmp: calculates the
+ * numeric difference between the first different characters in the strings
  * @s1: first string
  * @s2: second string
  *
- * Return: integer. 0 if equal strings, positive if s1 larger than s2,
- * negative if s1 shorter than s2
+ * Return: integer. 0 if equal strings, positive if s1 first different
+ * character is larger than s2's first different character,
+ * negative otherwise
  */
 
 int _strcmp(char *s1, char *s2)
