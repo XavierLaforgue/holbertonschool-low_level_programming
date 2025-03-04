992ccda968e25188f9983deaa52008eea3e3a04f
XavierLaforgue <xavierjimmys@gmail.com>
2025-03-04 23:02:00 +0100

    Testing similar code

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -1,36 +1,38 @@
 #include "main.h"
 
 /**
- * _sqrt - searches for the natural square root of a natural number
- * via recursive descending essays
- * @n: natural number whose natural square root will be calculated
- * @i: depth of the current attempt
- * Return: -1 if n is negative or if there is no natural square root; the
- * natural square root otherwise
+ * _sqrt_compute - find the sqrt
+ * @n: number to find sqrt
+ * @g: Current guess for sqrt
+ * Return: int sqrt
  */
 
-int _sqrt(int n, int i)
+int _sqrt(int n, int m)
 {
-	if (n - i < 0)
+	if (m * m == n)
+	{
+		return (m);
+	}
+	if (m * m > n)
+	{
 		return (-1);
-	else if (((n - i) * (n - i)) == n)
-		return (n - i);
-	else
-		return (_sqrt(n, i + 1));
+	}
+
+	return (_sqrt(n, m + 1));
 }
 
 /**
- * _sqrt_recursion - calculates the natural square root of a number
- * @n: natural number whose natural square root will be calculated
- * Return: the natural square root of n, unless there is none, in which
- * case it will return -1
+ * _sqrt_recursion - returns natural square root
+ * @n: number to check
+ * Return: int sqrt
  */
 
 int _sqrt_recursion(int n)
 {
-	int guess = 0;
-
-	if (n <= 0)
+	if (n < 0)
+	{
 		return (-1);
-	return (_sqrt(n, guess));
+	}
+
+	return (_sqrt(n, 1));
 }
---
+++ recursion/5-sqrt_recursion_mine.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * _sqrt - searches for the natural square root of a natural number
+ * via recursive descending essays
+ * @n: natural number whose natural square root will be calculated
+ * @i: depth of the current attempt
+ * Return: -1 if n is negative or if there is no natural square root; the
+ * natural square root otherwise
+ */
+
+int _sqrt(int n, int i)
+{
+	if (n - i < 0)
+		return (-1);
+	else if (((n - i) * (n - i)) == n)
+		return (n - i);
+	else
+		return (_sqrt(n, i + 1));
+}
+
+/**
+ * _sqrt_recursion - calculates the natural square root of a number
+ * @n: natural number whose natural square root will be calculated
+ * Return: the natural square root of n, unless there is none, in which
+ * case it will return -1
+ */
+
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+		return (-1);
+	return (_sqrt(n, 0));
+}
