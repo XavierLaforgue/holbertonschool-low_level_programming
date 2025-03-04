562abe32dc4af9747f5b3b56ee015b70c7aa5812
XavierLaforgue <xavierjimmys@gmail.com>
2025-03-04 17:41:02 +0100

    Function that returns the natural square root of a number

---
+++ recursion/6-is_prime_number.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * is_prime_number - returns 1 if input is a prime number, 0 otherwise
+ * @n: number to be tested
+ * Return: 1 if prime, 0 otherwise
+ */
+
+int is_prime_number(int n)
+{
+} 
