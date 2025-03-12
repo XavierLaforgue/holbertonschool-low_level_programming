8fe71430d0c0533008b83d823e3a30abd8a1135e
XavierLaforgue <xavierjimmys@gmail.com>
2025-03-12 17:50:41 +0100

    Structure that contains the characteristics of a dog

---
+++ structures_typedef/README.md
@@ -0,0 +1,1 @@
+Week 5, Project 3: Structures, typedef
---
+++ structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef MYHEADER_H
+#define MYHEADER_H
+
+/**
+ * struct dog - structure that contains the characeteristics of a dog
+ * @name: string, name of the dog
+ * @age: float, age of the dog
+ * @owner: string, name of the owner of the dog
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
