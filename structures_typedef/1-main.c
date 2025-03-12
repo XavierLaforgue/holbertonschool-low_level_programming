2a5dab5a230daa2d141a56ad7e56efb1f109d4b3
XavierLaforgue <xavierjimmys@gmail.com>
2025-03-12 18:14:14 +0100

    Include case where NULL struct is given

--- structures_typedef/1-main.c
+++ structures_typedef/1-main.c
@@ -13,5 +13,6 @@ int main(void)
 	init_dog(&my_dog, "Poppy", 3.5, "Bob");
 	printf("My name is %s, and I am %.1f :) - Woof!\n", my_dog.name, my_dog.age);
 	printf("My owner is %s, by the way\n", my_dog.owner);
+	init_dog(NULL, "Firulais", 5, "Tommy");
 	return (0);
 }
