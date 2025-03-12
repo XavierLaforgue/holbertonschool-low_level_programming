3670ad912ffd3282d91a33bf51bf1ecc88cc25a6
XavierLaforgue <xavierjimmys@gmail.com>
2025-03-12 15:14:03 +0100

    Function that allocates memory using malloc and exits with status value 98 if malloc fails

---
+++ more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * malloc_checked - allocates memory using malloc and terminate with chosen
+ * status value if it fails
+ * @b: number of bytes that me allocated
+ * Return: normal process termination with status value 98 if failure,
+ * pointer to allocated memory if success
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	void *ptr;
+	
+	ptr = malloc(b);
+	if (ptr == NULL)
+		exit(98);
+	return (ptr);
+}
---
+++ more_malloc_free/README.md
@@ -0,0 +1,1 @@
+Week 6, Project 2: more malloc, free
