af9aec748ae2b1c050b4749a37f1013178976928
XavierLaforgue <xavierjimmys@gmail.com>
2025-03-18 14:27:26 +0100

    Remove case in which compare function is NULL

--- function_pointers/2-int_index.c
+++ function_pointers/2-int_index.c
@@ -16,8 +16,6 @@ int int_index(int *array, int size, int (*cmp)(int))
 
 	if (size <= 0)
 		return (-1);
-	if (cmp == NULL)
-		return (-1);
 	for (i = 0; i < size; ++i)
 	{
 		is_it = cmp(array[i]);
