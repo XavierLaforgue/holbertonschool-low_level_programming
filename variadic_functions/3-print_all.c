a7b0e707952afea1f6da6e72dd0e30ce55155fa1
XavierLaforgue <xavierjimmys@gmail.com>
2025-03-20 17:16:20 +0100

    Revert fix

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -11,6 +11,7 @@ void print_all(const char * const format, ...)
 	va_list va;
 	char str_printf[3] = {'%', '\0', '\0'};
 	unsigned int n = 0;
+	int printed = 0;
 	char *str;
 	int allowed_format;
 
@@ -18,27 +19,29 @@ void print_all(const char * const format, ...)
 	while (format[n] != '\0')
 	{
 		allowed_format = format[n] == 'c' || format[n] == 's' ||
-							format[n] == 'i' || format[n] == 'f';
-		if (n != 0 && allowed_format)
+						format[n] == 'i' || format[n] == 'f';
+		if (n != 0 && printed > 0 && allowed_format)
 			printf(", ");
 		str_printf[1] = format[n];
 		switch (format[n])
 		{
 			case 'c':
-				printf(str_printf, va_arg(va, int));
+				printed = printf(str_printf, va_arg(va, int));
 				break;
 			case 'i':
-				printf(str_printf, va_arg(va, int));
+				printed = printf(str_printf, va_arg(va, int));
 				break;
 			case 'f':
-				printf(str_printf, va_arg(va, double));
+				printed = printf(str_printf, va_arg(va, double));
 				break;
 			case 's':
 				str = va_arg(va, char *);
 				if (str == NULL)
 					str = "(nil)";
-				printf(str_printf, str);
+				printed = printf(str_printf, str);
 				break;
+			default:
+				printed = 0;
 		}
 		++n;
 	}
