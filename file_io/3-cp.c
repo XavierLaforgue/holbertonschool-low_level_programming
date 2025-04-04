20a97ad19fcec5c289e7b3db22680c63b6f8c5bc
XavierLaforgue <xavierjimmys@gmail.com>
2025-04-04 22:42:33 +0200

    3-cp.c update
    revert to generalized  function for open and read

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -104,12 +104,8 @@ int main(int argc, char *argv[])
 
 	error_args(argc != 3);
 	fd_from = open(argv[1], O_RDONLY);
-	if (fd_from == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-		exit(98);
-	}
 	fd_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, file_to_mode);
+	err_fd_from(fd_from, b_cp_from, fd_to, argv[1]);
 	err_fd_to(fd_to, b_cp_to, fd_from, argv[2]);
 	while (b_cp_from > 0 && b_cp_to > 0)
 	{
