ee86bea3846462b565eee58921762364cfd58e13
XavierLaforgue <xavierjimmys@gmail.com>
2025-04-04 21:12:59 +0200

    3-cp.c update
    wrong exit code 99998 for reading

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -103,13 +103,13 @@ int main(int argc, char *argv[])
 
 	fd_from = open(argv[1], O_RDONLY);
 	if (fd_from == -1)
-		error_exit_str(98, "NOT THIS Error: Can't read from file %s\n", argv[1]);
+		error_exit_str(98, "Error: Can't read from file %s\n", argv[1]);
 
 	bytes_read = read(fd_from, buffer, BUFFER_SIZE);
 	if (bytes_read == -1)
 	{
 		close(fd_from);
-		error_exit_str(98, "Error: Can't read from file %s\n", argv[1]);
+		error_exit_str(99998, "Error: Can't read from file %s\n", argv[1]);
 	}
 
 	fd_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
