7abd3e98130d38b1dc9c5afd6e7ee08cb31a4b18
XavierLaforgue <xavierjimmys@gmail.com>
2025-04-04 16:07:38 +0200

    1-create_file.c update
    Close file if write operation fails.

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -39,6 +39,7 @@ int create_file(const char *filename, char *text_content)
 	bytes_written = write(fd, text_content, text_len);
 	if (bytes_written == -1)
 	{
+		close(fd);
 		printf("did not write in file\n");
 		return (-1);
 	}
